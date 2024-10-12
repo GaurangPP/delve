0bf24d5183b565e8f4977c20cdb48f2ed3513761
Gaurang Pendharkar <72285657+GaurangPP@users.noreply.github.com>
2024-10-12 14:22:45 -0700

    Added search bar and greeting

--- delve-app/src/components/Landing.jsx
+++ delve-app/src/components/Landing.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from 'react';
+import './Landing.css';
 
 export const Landing = () => {
   return (
-    <div>Landing</div>
-  )
-}
+    <div className='landing-container'>
+      <header className='header'>
+        <h2 className='greeting'>Good afternoon, Sadie!</h2>
+        <div className='search-container'>
+            <div className='search-bar'>
+                <i className='fas fa-search'></i>
+                <input type='text' />
+            </div>
+            <button className='add-button'>+</button>
+        </div>
+      </header>
+      <div className='event-list'>
+        <div className='event-item'>
+          <span className='event-date'>9</span>
+          <span className='event-name'>DubHacks!</span>
+        </div>
+        {/* Add more event items here */}
+      </div>
+    </div>
+  );
+};
+
+
