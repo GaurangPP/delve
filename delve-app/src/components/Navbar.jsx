55216257e6eb92a0d9f143d83425c7635181b897
incharachetan <inchara.chetan@gmail.com>
2024-10-12 14:24:13 -0700

    Navbar Modification

--- delve-app/src/components/Navbar.jsx
+++ delve-app/src/components/Navbar.jsx
@@ -1,51 +1,47 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
-import "./Navbar.css"
-import {useEffect, useState} from "react";
+import "./Navbar.css";
+import { useEffect, useState } from "react";
 
 export const Navbar = () => {
+  const [click, setClick] = useState(false);
 
-    const [click, setClick] = useState(false);
-    //const [button, setButton] = useState(true);
-
-    const handleClick = () => setClick(!click);
-    const closeMobileMenu = () => setClick(false);
-
-
-
-    //window.addEventListener('resize', showButton);
-
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
   return (
     <>
-            <nav className = "navbar">
-                <div className = "navbar-container">
-                    <Link to="/" className = "navbar-logo" onClick={closeMobileMenu}>
-                        Delve
-                    </Link>
-                    <div className="menu-icon" onClick={handleClick}>
-                        <i className={click ? 'fa-solid fa-times' : 'fa-solid fa-bars'}></i>
-                    </div>
-                    
-                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                        <li className="nav-item">
-                            <Link to='/' className="nav-links" onClick={closeMobileMenu}>
-                                Home
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to='/moodlog' className="nav-links" onClick={closeMobileMenu}>
-                                Moodlog
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to='/resources' className="nav-links" onClick={closeMobileMenu}>
-                                Resources
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </>
-  )
+      <nav className="navbar">
+        <div className="navbar-header">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            Delve
+          </Link>
+        </div>
+
+        <div className="navbar-container">
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? 'fa-solid fa-times' : 'fa-solid fa-bars'}></i>
+          </div>
+
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className="nav-item">
+              <Link to='/' className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to='/moodlog' className="nav-links" onClick={closeMobileMenu}>
+                Moodlog
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to='/resources' className="nav-links" onClick={closeMobileMenu}>
+                Resources
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
 }
